a1b403c2084a0053df6eafed75932e3e5c106939
Developer <developer@seans-air-2.attlocal.net>
2014-12-07 19:06:14 -0800

    another commit

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -10,6 +10,8 @@ Rails.application.configure do
   # Rake tasks automatically ignore this option for performance.
   config.eager_load = true
 
+  config.assets.paths << Rails.root.join('vendor', 'assets')
+
   # Full error reports are disabled and caching is turned on.
   config.consider_all_requests_local       = true
   config.action_controller.perform_caching = true
