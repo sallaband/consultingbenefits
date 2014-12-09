836e2542acaf82bfed92335aab65b91d26c9d886
Developer <developer@Seans-MacBook-Air-2.local>
2014-12-08 17:43:18 -0800

    production.rb update

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,22 +43,23 @@ Rails.application.configure do
                                    "fontawesome-webfont.woff")
 
 
+config.assets.precompile << Proc.new do |path|
+  if path =~ /\.(css|js)\z/
+    full_path = Rails.application.assets.resolve(path).to_path
+    app_assets_path = Rails.root.join('app', 'assets').to_path
+    vendor_assets_path = Rails.root.join('vendor', 'assets').to_path
 
-  config.assets.precompile << Proc.new do |path|
-        if path =~ /\.(css|js)\z/
-          full_path = Rails.application.assets.resolve(path).to_path
-          app_assets_path = Rails.root.join('app', 'assets').to_path
-          if full_path.starts_with? app_assets_path
-            puts "including asset: " + full_path
-            true
-          else
-            puts "excluding asset: " + full_path
-            false
-          end
-        else
-          false
-        end
-      end
+    if ((full_path.starts_with? app_assets_path) || (full_path.starts_with? vendor_assets_path)) && (!path.starts_with? '_')
+      puts "including asset: " + full_path
+      true
+    else
+      puts "excluding asset: " + full_path
+      false
+    end
+  else
+    false
+  end
+end
 
   # `config.assets.precompile` and `config.assets.version` have moved to config/initializers/assets.rb
 
