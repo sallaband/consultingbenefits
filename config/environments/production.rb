1fdcdbe6e8d3e7fa96db95e4e2729b9bed9a6161
Developer <developer@seans-air-2.attlocal.net>
2014-12-10 19:16:01 -0800

    cb.com to gmail.com

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -43,7 +43,7 @@ Rails.application.configure do
   config.action_mailer.smtp_settings = {
     address:              'smtp.gmail.com',
     port:                 587,
-    domain:               'consultingbenefits.com',
+    domain:               'gmail.com',
     user_name:            'seanthefinanceguy@gmail.com',
     password:             'Football99',
     authentication:       'plain',
